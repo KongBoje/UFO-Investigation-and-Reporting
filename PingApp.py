25db98151fcbe9f4f1b0fddbc9938880d0208caf
Michael <Michael_veilis@hotmail.com>
2018-10-24 12:00:17 +0200

    lavet exercise

--- PingApp.py
+++ PingApp.py
@@ -1,31 +1,29 @@
-import timeit
+import subprocess
 
-URLSERVERS = [
-    'http://128.199.144.199:8080',
-    'http://167.99.51.33:8080',
-    'http://46.101.253.149:8080'
-]
+def ping_return(hostname):
+    amount = 100
+    avg = 0
+    result = subprocess.check_output("ping " + hostname, amount).decode("utf-8")
+    splitCode = result.split("\n")
+    for sc in splitCode:
+        if "time=" in sc:
+            idx = sc.find('time')
+            avg += float(sc[idx+5:-10])
+    return round((avg/amount),2)
 
-snippet = '''
-import requests
-requests.get('{}')
-'''
+def the_hosts(list_of_hosts):
+    return_sc = dict()
+    for hostname in list_of_hosts:
+        return_sc[hostname] = ping_return(hostname)
 
-def take_ping_time(snippet, iteration):
-    setup = '''print()'''
-
-    time_total = timeit.timeit(setup = setup,
-                               stmt = snippet,
-                               number = iteration)
-
-    average_time = time_total / iteration
-
-    print('time_total = {}'.format(time_total))
-    print('average_time = {}'.format(average_time))
+    return return_sc
 
 def main():
-    for URL in URLSERVERS:
-        take_ping_time(snippet.format(URL), 100)
+    test_hosts = ['128.199.144.199', '167.99.51.33', '46.101.253.149']
+    for x in test_hosts:
+        ping_return(x)
+
+    print(the_hosts(test_hosts))
 
 if __name__ == '__main__':
     main()
